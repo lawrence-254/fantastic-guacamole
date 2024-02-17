96098b303a68f88f14e2f029e871e1d5a482fe2d
lawrence-254 <lodhiambo26@gmail.com>
2024-02-17 11:36:58 +0300

    blueprint

---
+++ main/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+from main.db import get_db
+
+bp = Blueprint('auth',__name__,url_prefix='/auth')
