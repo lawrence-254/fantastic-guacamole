10eebe3696fed67ce9cbf614198d2b594f1388a9
lawrence-254 <lodhiambo26@gmail.com>
2024-04-10 21:33:31 +0300

    pages

--- 1/genesis/src/components/Layout.jsx
+++ 1/genesis/src/components/Layout.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+import Navbar from './Navbar';
+import Footer from './Footer';
 
+const OuterLayout = styled.div`
+
+`;
+const InnerLayout = styled.div`
+`;
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <OuterLayout>
+        <Navbar/>
+        <InnerLayout>
+
+        </InnerLayout>
+        <Footer/>
+    </OuterLayout>
   )
 }
 
