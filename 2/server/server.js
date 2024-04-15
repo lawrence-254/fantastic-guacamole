0e5bf6ea9cd040a90ce79d3b29c1a478d6be996f
lawrence-254 <lodhiambo26@gmail.com>
2024-04-15 19:00:46 +0300

    dbinit

--- 2/server/server.js
+++ 2/server/server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const sequelize = require('./config/db');
+const User = require('./models/userModels');
+const Todo = require('./models/todosModels');
 
-//routes
-const userRoutes = require('./routes/userRoutes');
-const todoRoutes = require('./routes/todoRoutes');
+async function syncDatabase() {
+    try {
+        await sequelize.sync({ alter: true });
+        console.log('Database synchronized successfully');
+    } catch (error) {
+        console.error('Error syncing database:', error);
+    }
+}
 
+syncDatabase();
 
-
-
-
-
-
-
+// Start your Express app and define routes here
 
 const PORT = process.env.PORT || 3000;
 
