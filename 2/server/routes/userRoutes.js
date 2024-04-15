1c7523b5c1d1b67c8e81746011296529cdbe55b7
lawrence-254 <lodhiambo26@gmail.com>
2024-04-15 19:11:34 +0300

    routes

--- 2/server/routes/userRoutes.js
+++ 2/server/routes/userRoutes.js
@@ -1,6 +1,39 @@
-// const express = require('express');
+const express = require('express');
 
-// const userRoutes = express.Router();
-// const db = require('../db/database');
+const userRoutes = express.Router();
+const sequelize = require('../config/db');
 
-// const Re
+const getAllUsers = async (req, res) => {
+    const users = await sequelize.models.User.findAll();
+    res.json(users);
+}
+
+const registerUser = async (req, res) => {
+    const { username, email, password } = req.body;
+    try {
+        const user = await sequelize.models.User.create({ username, email, password });
+        res.json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+const getUserById = async (req, res) => {
+    const { id } = req.params;
+    const user = await sequelize.models.User.findByPk(id);
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).json({ error: 'User not found' });
+    }
+}
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await sequelize.models.User.findOne({ where: { email, password } });
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(401).json({ error: 'Invalid email or password' });
+    }
+}
