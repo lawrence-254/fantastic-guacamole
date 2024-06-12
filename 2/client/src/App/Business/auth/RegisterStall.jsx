7077ad773ba467ddb692cf9fbc1fdbe7e5fe2088
lawrence-254 <lodhiambo26@gmail.com>
2024-06-12 10:12:51 +0300

    setup

--- 2/client/src/App/Business/auth/RegisterStall.jsx
+++ 2/client/src/App/Business/auth/RegisterStall.jsx
@@ -16,18 +16,18 @@ const RegisterStall = () => {
                 <input className='form0control' placeholder='Enter a unique business Id'/>
             </div>
             <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" className="form-control" id="password" placeholder='Enter password' onChange={handleChange}/>
-          </div>
-           <div className="form-group">
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input type="password" className="form-control" id="confirmPassword" placeholder='Enter password confirmation' onChange={handleChange}/>
-          </div>
-          <button type="submit" className="btn btn-primary" disabled={loading}>
-            {loading ? 'REGISTERING...' : 'REGISTER'}
+                <label htmlFor="password">Password</label>
+                <input type="password" className="form-control" id="password" placeholder='Enter password' onChange={handleChange}/>
+            </div>
+            <div className="form-group">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input type="password" className="form-control" id="confirmPassword" placeholder='Enter password confirmation' onChange={handleChange}/>
+            </div>
+            <button type="submit" className="btn btn-primary" disabled={loading}>
+                {loading ? 'REGISTERING...' : 'REGISTER'}
             </button>
         </form>
-         <p>Have an account? <Link to="/register">REGISTER</Link></p>
+        <p>Have an account? <Link to="/login">LOGIN</Link></p>
     </div>
   )
 }
