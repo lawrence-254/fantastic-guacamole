533fb32c63d54812cb671d5025d3d359117e7fd7
lawrence-254 <lodhiambo26@gmail.com>
2024-06-14 13:25:47 +0300

    4

--- 2/client/src/App/Business/pages/StallProfile.jsx
+++ 2/client/src/App/Business/pages/StallProfile.jsx
@@ -5,38 +5,50 @@ import Layout from '../Components/Layout/Layout'
 const StallProfile = () => {
   const [loading, setLoading]=useState(false);
 
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const form = document.forms.addProfile;
+    const profile = {
+      stallTitle: form.stallTitle.value, description: form.description.value,
+      phoneNumber: form.phoneNumber.value, email: form.email.value, location:form.location.value,
+      profileImage: form.profileImage.value
+    }
+    console.log(profile);
+  }
+
   return (
     <Layout>
       <h1>STALL PROFILE</h1>
       <p>Show the details to your stall</p>
       <div>
         <h2>Add your stall details</h2>
-        <form action="" method="post">
+        <form  name="addProfile" method="post">
           <div className="form-group">
             <label htmlFor="shopTitle">Stall Title</label>
-            <input type="text" name="" id="" />
+            <input type="text" name="stallTitle" id="" />
           </div>
           <div className="form-group">
             <label htmlFor="description">Describe what your stall deals in (electronics, shoes, etc...)</label>
-            <input type="text" name="" id="" />
+            <input type="text" name="description" id="" />
           </div>
           <div className="form-group">
             <label htmlFor="phoneNumber">Enter phone number</label>
-            <input type="tel" name="" id="" />
+            <input type="tel" name="phoneNumber" id="" />
           </div>
           <div className="form-group">
             <label htmlFor="email">Enter email</label>
-            <input type="email" name="" id="" />
+            <input type="email" name="email" id="" />
           </div>
           <div className="form-group">
             <label htmlFor="location">Enter your business location address</label>
-            <input type="text" name="" id="" />
+            <input type="text" name="location" id="" />
           </div>
           <div className="form-group">
             <label htmlFor="profileImage">Stall Profile Image</label>
-            <input type="file" name="" id="" />
+            <input type="file" name="profileImage" id="" />
           </div>
-          <button type="submit" className="btn btn-primary" disabled={loading}>
+          <button type="submit" className="btn btn-primary" disabled={loading} onClick={handleSubmit}>
             {loading ? 'UPDATING...' : 'UPDATE'}
         </button>
         </form>
